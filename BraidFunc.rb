7ecc49072fa4df43b4f07adfa642be170bc5bf0a
ConorOBrien <csobrien@email.wm.edu>
2021-04-20 06:13:13 -0400

    initial commit

---
+++ BraidFunc.rb
@@ -0,0 +1,5 @@
+# Apr. 20, 2021. 06:11
+# Okay so I just spent the last 2 hours staring at the Toadskin Source code
+# And I kinda just went no, I can't be fucked.
+# (And no, I don't smoke, despite the date.)
+
