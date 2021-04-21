8c7b1eed610f6fb9aff872eb7d1186d01e172592
ConorOBrien <csobrien@email.wm.edu>
2021-04-21 03:26:09 -0400

    implement ReadStream.rb

--- BraidFunc.rb
+++ BraidFunc.rb
@@ -3,3 +3,13 @@
 # And I kinda just went no, I can't be fucked.
 # (And no, I don't smoke, despite the date.)
 
+# Apr. 21, 2021. 03:13
+# Work begins
+require_relative "ReadStream.rb"
+
+# a = ReadStream.new STDIN
+a = ReadStream.new File.open "README.md"
+
+13.times {
+    puts a.read 1
+}
